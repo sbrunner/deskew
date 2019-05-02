d49e46e9727cfbc0de6f42b7a9e24c628a00add7
CI <ci@ci.com>
2019-05-02 18:41:47 +0000

    [skip ci] Bump Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='deskew',
-    version='0.9.0',
+    version='0.10.0',
     description='Skew detection and correction in images containing text',
     long_description="""Skew detection and correction in images containing text
 
