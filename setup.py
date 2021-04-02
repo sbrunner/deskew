15cad49e2669e9c5f92851f046d5fb186aeff490
CI <ci@ci.com>
2021-04-02 05:17:58 +0000

    [skip ci] Bump Version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 setup(
     name="deskew",
-    version="0.10.19",
+    version="0.10.20",
     description="Skew detection and correction in images containing text",
     long_description="""Skew detection and correction in images containing text
 
